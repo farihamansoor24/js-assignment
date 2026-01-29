58ba88cd869252c12ecad0a393e6a6f9fec14708
Fariha Mansoor <you@example.com>
2026-01-29 21:17:04 +0500

    add new files

--- chapter-01/index.js
+++ chapter-01/index.js
@@ -1,2 +1,15 @@
-// Write a script to greet your website visitor using JS alert box.
+// 1: Write a script to greet your website visitor using JS alert box.
 alert('Hello Fariha Mansoor');
+
+// 2: Write a script to display following message on your web page: 
+alert('Error! Please Enter a Valid Password');
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break) 
+alert('Welcome to JS land.. \n Happy Coding');
+
+// 4. Write a script to display following messages in sequence: 
+alert('Welcome to JS land..');
+alert('Happy Coding');
+
+// 5. Generate the following message through browser’s developer console: 
+console.log("Hello.. I can run JS through my web browser's console");
